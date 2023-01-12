3de255bc558bfa8aa137700f4eacbd2bc4ba2d7b
DerpyCrabs <derpycrabs@gmail.com>
2023-01-12 15:59:24 +0300

    Add more tests for loggingMiddleware

--- tests/loggingMiddleware.test.ts
+++ tests/loggingMiddleware.test.ts
@@ -79,3 +79,50 @@ test('logOutputFn in afterTransact receives correct stateBefore', async () => {
     expect.objectContaining({ stateBefore: { field: 5 }, stateAfter: { field: 10 } })
   )
 })
+
+test('diff = false disables diff printing', () => {
+  const logOutputFnSpy = vi.fn()
+  const db = new JsonDB(
+    { field: 5 },
+    {
+      middleware: loggingMiddleware({
+        logOutputFn: logOutputFnSpy,
+        diff: false,
+      }),
+    }
+  )
+  db.transact({ test: 'field' })(state => {
+    state.test = 10
+  })
+  expect(logOutputFnSpy).toHaveBeenCalledTimes(1)
+  expect(logOutputFnSpy).toHaveBeenCalledWith(
+    expect.objectContaining({
+      stateBefore: { field: 5 },
+      stateAfter: { field: 10 },
+      message: `afterTransact: {"test":"field"}`,
+    })
+  )
+})
+
+test('logMigrate, logTransact, logGetSnapshot = false disable logging of these methods', async () => {
+  const logOutputFnSpy = vi.fn()
+  const db = await new JsonDB(
+    { field: 5 },
+    {
+      middleware: loggingMiddleware({
+        logOutputFn: logOutputFnSpy,
+        logTransact: false,
+        logMigrate: false,
+        logGetSnapshot: false,
+      }),
+    }
+  )
+    .migrate('sync migration', state => state)
+    .migrateAsync('async migration', async state => state)
+  db.transact({})(() => {})
+  await db.transactAsync({})(async () => {})
+  db.getSnapshot()
+  await db.getSnapshotAsync()
+
+  expect(logOutputFnSpy).toHaveBeenCalledTimes(0)
+})
