b5368b54e5ad6152c278225cdd77eeeaad57f17d
DerpyCrabs <derpycrabs@gmail.com>
2023-01-28 14:29:57 +0300

    Add strictWrites flag to filePersistenceMiddleware

--- src/middlewares/filePersistenceMiddleware.ts
+++ src/middlewares/filePersistenceMiddleware.ts
@@ -1,10 +1,21 @@
 import type { JsonDBMiddleware } from '..'
 import fs from 'fs'
 
-export default function filePersistenceMiddleware<Schema>(filePath: string): JsonDBMiddleware<Schema> {
+export default function filePersistenceMiddleware<Schema>(
+  filePath: string,
+  strictWrites: boolean = true
+): JsonDBMiddleware<Schema> {
+  let hasReadDataFromDisk = false
+  let asyncWriteQueue: any[] = []
+  let asyncWriterHandle: null | NodeJS.Timeout = null
+
   const beforeFn = ({ stateBefore }: any) => {
+    if (hasReadDataFromDisk) {
+      return stateBefore
+    }
     try {
       const fileContents = fs.readFileSync(filePath, { encoding: 'utf-8' })
+      hasReadDataFromDisk = true
       return JSON.parse(fileContents)
     } catch (e: any) {
       if ('code' in e && e.code === 'ENOENT') {
@@ -16,8 +27,12 @@ export default function filePersistenceMiddleware<Schema>(filePath: string): Jso
   }
 
   const beforeFnAsync = async ({ stateBefore }: any) => {
+    if (hasReadDataFromDisk) {
+      return stateBefore
+    }
     try {
       const fileContents = await fs.promises.readFile(filePath, { encoding: 'utf-8' })
+      hasReadDataFromDisk = true
       return JSON.parse(fileContents)
     } catch (e: any) {
       if ('code' in e && e.code === 'ENOENT') {
@@ -34,7 +49,25 @@ export default function filePersistenceMiddleware<Schema>(filePath: string): Jso
   }
 
   const afterFnAsync = async ({ stateAfter }: any) => {
-    await fs.promises.writeFile(filePath, JSON.stringify(stateAfter), { encoding: 'utf-8' })
+    if (strictWrites) {
+      await fs.promises.writeFile(filePath, JSON.stringify(stateAfter), {
+        encoding: 'utf-8',
+      })
+    } else {
+      asyncWriteQueue.push(stateAfter)
+      if (!asyncWriterHandle) {
+        asyncWriterHandle = setTimeout(async () => {
+          if (asyncWriteQueue.length !== 0) {
+            await fs.promises.writeFile(filePath, JSON.stringify(asyncWriteQueue[asyncWriteQueue.length - 1]), {
+              encoding: 'utf-8',
+            })
+          }
+          asyncWriterHandle = null
+          asyncWriteQueue = []
+        }, 0)
+      }
+    }
+
     return stateAfter
   }
 
